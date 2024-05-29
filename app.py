ace087d3ffa9a1b6c23d94810fbc830e0c3ffffc
leoneville <leoneville_@hotmail.com>
2024-05-28 21:51:35 -0300

    feat: Implement Pix payment method
    Integrate Pix as a payment option, enabling users to generate and retrieve Pix QR codes for seamless payment processing.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta, timezone
-from flask import Flask, jsonify, request
+
+from flask import Flask, jsonify, request, send_file
+
+from src.payments.pix import Pix
 from src.repository.database import db
 from src.models.payment import Payment
 
@@ -19,6 +22,11 @@ def create_payment_pix():
     expiration_date = datetime.now(timezone.utc) + timedelta(minutes=30)
 
     new_payment = Payment(value=data["value"], expiration_date=expiration_date)
+
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
     
     db.session.add(new_payment)
     db.session.commit()
@@ -27,6 +35,10 @@ def create_payment_pix():
         "message": "The payment has been created.",
         "payment": new_payment.to_dict()
     })
+
+@app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
+def get_image(file_name: str):
+    return send_file(f"static/img/{file_name}.png", mimetype="image/png")
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
